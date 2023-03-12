dd9226c5088e22331ecff0c1a5a13a0f01d64c4b
Emily Keane <ekeane05@qub.ac.uk>
2023-03-12 16:59:35 +0000

    1dt commit

---
+++ source/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,418 @@
+package org.jfree.data;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Range;
+import org.jfree.data.Values2D;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DataUtilitiesTest {
+	private Values2D values2D;
+
+	@Before
+	public void setUp() {
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(3, 1, 0);
+		testValues.addValue(3, 2, 0);
+		testValues.addValue(2, 0, 1);
+		testValues.addValue(2, 1, 1);
+		testValues.addValue(4, 2, 1);
+		testValues.addValue(3, 0, 2);
+		testValues.addValue(3, 1, 2);
+		testValues.addValue(3, 2, 2);
+
+	}
+
+	@After
+	public void tearDown() {
+		values2D = null;
+	}
+
+	// Test case failed due to invalid column index- should return 0 for invalid
+	// input
+	@Test
+	public void testValidDataAndInValidNegativeColumnIndex() {
+		try {
+			assertEquals("Sum returned should be 0", 0, DataUtilities.calculateColumnTotal(values2D, -1), 0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			fail("An unexpected IndexOutOfBoundsException was thrown");
+		}
+	}
+
+//TC failed - method does not return an exception
+	@Test
+	public void testNullDataandColumnIndex() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 1);
+			fail("No exception thrown. The expected outcome was: a thrown "
+					+ "exception of type: IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(NullPointerException.class));
+		}
+	}
+
+	@Test
+	public void testValidDataAndZeroColumnIndex() {
+		assertEquals("Sum returned should be 7.0", 7, DataUtilities.calculateColumnTotal(values2D, 0), 0.0000001d);
+	}
+
+	@Test
+	public void testValidDataAndValidPositiveColumnIndex() {
+		assertEquals("Sum returned should be 8.0", 8, DataUtilities.calculateColumnTotal(values2D, 1), 0.0000001d);
+	}
+
+	@Test
+	public void test0x0DataAndPositiveColumnIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		assertEquals("Sum returned should be 0.0", 0, DataUtilities.calculateColumnTotal(values2D, 1), 0.0000001d);
+	}
+
+//Test case failed due to invalid column index -1 should return 0 for invalid input
+	@Test
+	public void test0x0DataAndNegativeColumnIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		try {
+			assertEquals("Sum returned should be 0.0", 0.0, DataUtilities.calculateColumnTotal(values2D, -1),
+					0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			fail("An unexpected IndexOutOfBoundsException was thrown");
+		}
+
+	}
+
+//Test case passed
+	@Test
+	public void test0x0DataAndZeroColumnIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		try {
+			assertEquals("Sum returned should be 0.0", 0.0, DataUtilities.calculateColumnTotal(values2D, 0),
+					0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			fail("An unexpected IndexOutOfBoundsException was thrown");
+		}
+
+	}
+	// rows now
+
+//Test case failed due to invalid column index- should return 0 for invalid input
+	@Test
+	public void testValidDataAndInValidNegativeRowIndex() {
+		try {
+			assertEquals("Sum returned should be 0", 0, DataUtilities.calculateRowTotal(values2D, -1), 0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			
+		}
+	}
+
+	// TC failed - method does not return an exception
+	@Test
+	public void testNullDataandRowIndex() {
+		try {
+			DataUtilities.calculateColumnTotal(null, 1);
+			fail("No exception thrown. The expected outcome was: a thrown "
+					+ "exception of type: IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(NullPointerException.class));
+		}
+	}
+
+//Failed - method should return 6 but is returning 8 
+	@Test
+	public void testValidDataAndZeroRowIndex() {
+		assertEquals("Sum returned should be 6", 6, DataUtilities.calculateRowTotal(values2D, 0), 0.0000001d);
+	}
+
+	// Failed - method should return 6 but is returning 7
+	@Test
+	public void testValidDataAndValidPositiveRowIndex() {
+		assertEquals("Sum returned should be 6.0", 8, DataUtilities.calculateRowTotal(values2D, 1), 0.0000001d);
+	}
+
+	@Test
+	public void test0x0DataAndPositiveRowIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		double a = DataUtilities.calculateColumnTotal(values2D, 1);
+		assertEquals("Sum returned should be 0.0", 0, DataUtilities.calculateColumnTotal(values2D, 1), 0.0000001d);
+	}
+
+//Test case failed due to invalid column index-1 should return 0 for invalid input
+	@Test
+	public void test0x0DataAndNegativeRowIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		try {
+			assertEquals("Sum returned should be 0.0", 0.0, DataUtilities.calculateColumnTotal(values2D, -1),
+					0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			fail("An unexpected IndexOutOfBoundsException was thrown");
+		}
+
+	}
+
+//Test case passed
+	@Test
+	public void test0x0DataAndZeroRowIndex() {
+		// set up
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(0, 0, 0);
+		testValues.addValue(0, 1, 0);
+		testValues.addValue(0, 2, 0);
+		testValues.addValue(0, 0, 1);
+		testValues.addValue(0, 1, 1);
+		testValues.addValue(0, 2, 1);
+		testValues.addValue(0, 0, 2);
+		testValues.addValue(0, 1, 2);
+		testValues.addValue(0, 2, 2);
+		try {
+			assertEquals("Sum returned should be 0.0", 0.0, DataUtilities.calculateRowTotal(values2D, 0),
+					0.0000001d);
+		} catch (IndexOutOfBoundsException e) {
+			fail("An unexpected IndexOutOfBoundsException was thrown");
+		}
+
+	}
+
+	// Test Cases for createNumberArray
+	@Test
+	public void testCreateEmptyNumberArray() {
+     
+		double[] data = {};
+		DataUtilities.createNumberArray(data);
+
+	}
+
+	@Test
+	public void testCreateNumberArray_ValidDataObjectWithMultipleValues() {
+		double[] input = { 1.0, 2.0, 3.0, 4.0 };
+		Number[] expectedOutput = { 1.0, 2.0, 3.0, 4.0 };
+		Number[] test = DataUtilities.createNumberArray(input);
+		assertArrayEquals(expectedOutput, test);
+	}
+
+	@Test
+	public void testCreateNumberArray_ValidDataObjectWithOnlyOneValuesInArray() {
+		double[] input = { 1.0 };
+		Number[] expectedOutput = { 1.0 };
+		Number[] test = DataUtilities.createNumberArray(input);
+		assertArrayEquals(expectedOutput, test);
+	}
+
+	@Test
+	public void testCreateNumberArray_ValidDataObjectWithMultipleValuesNegative() {
+		double[] input = { -1.0, -2.0, -3.0 };
+		Number[] expectedOutput = { -1.0, -2.0, -3.0 };
+		Number[] test = DataUtilities.createNumberArray(input);
+		assertArrayEquals(expectedOutput, test);
+	}
+
+	@Test
+	public void testCreateNumberArray_MaximumValidInputValue() {
+		double[] input = new double[1000];
+		Arrays.fill(input, Double.MAX_VALUE);
+		Number[] expectedOutput = new Number[1000];
+		Arrays.fill(expectedOutput, Double.MAX_VALUE);
+		Number[] test = DataUtilities.createNumberArray(input);
+		assertArrayEquals(expectedOutput,test );
+	}
+
+//Test Cases for 2dcreateNumberArray
+	@Test
+	public void testCreateNumberArray2D() {
+		// Test valid input
+		double[][] validData1 = { { 1.0, 2.0, 3.0 }, { 4.0, 5.0, 6.0 } };
+		Number[][] test = DataUtilities.createNumberArray2D(validData1);
+		Number[][] expectedValidResult1 = { { 1.0, 2.0, 3.0 }, { 4.0, 5.0, 6.0 } };
+		assertArrayEquals(expectedValidResult1, test);
+
+	}
+
+	@Test
+	public void testCreateNumberArray2Dnegative() {
+		// Test valid input
+		double[][] validData1 = { { -1.0, -2.0, -3.0 }, { -4.0, -5.0, -6.0 } };
+		Number[][] test = DataUtilities.createNumberArray2D(validData1);
+		Number[][] expectedValidResult1 = { { -1.0, -2.0, -3.0 }, { -4.0, -5.0, -6.0 } };
+		assertArrayEquals(expectedValidResult1, test);
+
+	}
+
+	@Test
+	public void testCreateNumberArray2DInvalidDataTwoDifferentSizesOfArray() {
+		double[][] data = { { 1.0, 2.0, 3.0 }, { 4.0, 5.0 } };
+		try {
+			Number[][] result = DataUtilities.createNumberArray2D(data);
+			fail("Expected InvalidParameterException was not thrown");
+		} catch (InvalidParameterException e) {
+			// expected exception
+		}
+	}
+
+	@Test
+	public void testCreateNumberArray2DInvalidData2EmptyArraysAssigned() {
+		double[][] data = { {}, {} };
+		try {
+			Number[][] result = DataUtilities.createNumberArray2D(data);
+			fail("Expected InvalidParameterException was not thrown");
+		} catch (InvalidParameterException e) {
+			assertTrue("Incorrect exception type thrown", e.getClass().equals(InvalidParameterException.class));
+		}
+	}
+
+	@Test
+	public void testCreateNumberArray2D_nvalidDataOnlyOneEmptyArrayAssigned() {
+		double[][] invalidData1 = {};
+		try {
+			DataUtilities.createNumberArray2D(invalidData1);
+			fail("Expected InvalidParameterException");
+		} catch (InvalidParameterException e) {
+			// Exception caught as expected
+		}
+	}
+
+	
+	  
+	  
+	   
+	  @Test public void testValidInputForCumulativePercentagesOnlyOneValue() { // Test case with valid input
+	  DefaultKeyedValues testValues = new DefaultKeyedValues(); 
+	  
+	  
+	  testValues.addValue("A", 2); 
+	   
+	  KeyedValues result = DataUtilities.getCumulativePercentages(testValues);
+	  
+	  assertEquals(1, result.getValue(0)); 
+	  
+	  }
+	  @Test public void testValidInputForCumaltivePercentagesDecimalPoints() { // Test case with valid input
+		  DefaultKeyedValues testValues = new DefaultKeyedValues(); 
+		  
+		  
+		  testValues.addValue("A", 2.5); 
+		  testValues.addValue("B", 2.5);
+		  testValues.addValue("C", 4); 
+		  KeyedValues result = DataUtilities.getCumulativePercentages(testValues);
+		  double expected = 5/9;
+		  assertEquals(expected, result.getValue(1)); 
+		  
+		  }
+	  
+	  @Test public void testValidInputForCumaltivePercentagesPositiveValues() { // Test case with valid input
+		  DefaultKeyedValues testValues = new DefaultKeyedValues(); 
+		  
+		  
+		  testValues.addValue("A", 2); 
+		  testValues.addValue("B", 2);
+		  testValues.addValue("C", 4); 
+		  KeyedValues result = DataUtilities.getCumulativePercentages(testValues);
+		  
+		  assertEquals(0.5, result.getValue(2)); 
+		  
+		  }
+	  
+	  @Test public void testValidInputForCumaltivePercentagesNegativeValues() { // Test case with valid input
+		  DefaultKeyedValues testValues = new DefaultKeyedValues(); 
+		  
+		  
+		  testValues.addValue("A", -2); 
+		  testValues.addValue("B", -2);
+		  testValues.addValue("C", -4); 
+		  KeyedValues result = DataUtilities.getCumulativePercentages(testValues);
+		  
+		  assertEquals(1.0, result.getValue(2)); 
+		  
+		  }
+	  
+	  
+	  
+	  @Test public void testInvalidInputNullForCumaltivePercentages() { // Test case with valid input
+		  DefaultKeyedValues testValues = new DefaultKeyedValues(); 
+		  
+		  try {
+			  testValues.addValue("A", null); 
+			  testValues.addValue("B", null);
+			  testValues.addValue("C", null); 
+			  KeyedValues result = DataUtilities.getCumulativePercentages(testValues);
+			  
+			  assertEquals(0.25, result.getValue(0)); 
+				fail("No exception thrown.");
+			} catch (AssertionError  e) {
+				
+		  
+		  
+		  }
+	  
+	 
+	 
+}}
---
+++ source/org/jfree/data/RangeTest.java
@@ -0,0 +1,390 @@
+package org.jfree.data;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.Range; 
+import org.junit.*; 
+ 
+public class RangeTest { 
+ 
+ private Range rangeObjectUnderTest;
+
+ 
+ 
+ 
+ @Before 
+ public void setUp(){ 
+	 rangeObjectUnderTest = new Range(1,1);
+     
+ 
+ }
+ 
+ @After 
+ public void tearDown(){ 
+	 rangeObjectUnderTest = null;
+ } 
+ 
+ 
+ //Shift test cases
+ 
+ //Moves all values to right by 5 in rangeObjectUnderTest and 
+ // allows zero crossing
+ //passes
+ @Test
+ public void testShiftPositiveDeltaWithAllowZeroCrossingTrue() {
+	 rangeObjectUnderTest = new Range(-1, 5);
+	 Range test2 = new Range(4, 10);
+	 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, 5,true);
+	 assertEquals("The new shifted range should be as expected", 
+			 test2, rangeObjectUnderTest);
+	 
+	   
+ }
+ 
+ 
+//Moves all values to left by -5 in rangeObjectUnderTest and 
+// allows zero crossing
+//passes
+ @Test
+ public void testShiftNegativeDeltaWithAllowZeroCrossingTrue() {
+	 rangeObjectUnderTest = new Range(1, 5);
+	 Range test2 = new Range(-4, 0);
+	 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, -5,true);
+	 assertEquals("The new shifted range should be as expected",  
+			 test2, rangeObjectUnderTest);
+	 
+	   
+ }
+ 
+ 
+//Moves all values move by 0 in rangeObjectUnderTest and 
+//allows zero crossing
+//passes
+ @Test
+ public void testShiftByZero() {
+	 rangeObjectUnderTest = new Range(1, 5);
+	 Range test2 = new Range(1, 5);
+	 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, 0,true);
+	 assertEquals("The new shifted range should be as expected",  
+			 test2, rangeObjectUnderTest);
+	 
+	   
+ }
+ 
+//Moves all values to right by 5 in rangeObjectUnderTest and 
+//doesn't allow zero crossing
+//fails
+ @Test
+ public void testShiftPositiveDeltaWithAllowZeroCrossingFalse() {
+	 rangeObjectUnderTest = new Range(-1, 5);
+	 Range test2 = new Range(0, 10);
+	 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, 5,false);
+	 assertEquals("The new shifted range should be as expected",  
+			 test2, rangeObjectUnderTest);
+	 
+	   
+ }
+ 
+//Moves all values to left by -5 in rangeObjectUnderTest and 
+// doesn't allows zero crossing
+//passes
+ @Test
+ public void testShiftNegativeDeltaWithAllowZeroCrossingFalse() {
+	 rangeObjectUnderTest = new Range(-1, 5);
+	 Range test2 = new Range(-6, 0);
+	 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, -5,false);
+	 assertEquals("The new shifted range should be as expected",  
+			 test2, rangeObjectUnderTest);
+	 
+	   
+ }
+//test to see if shift() allows null values
+//passes
+ @Test
+ public void testShiftWhenNullValueIsEntered() throws Exception {
+	 rangeObjectUnderTest = null;
+	 try {
+		 rangeObjectUnderTest = Range.shift(rangeObjectUnderTest, -5,false);
+	        fail("Expected NullPointerException was not thrown");
+	    } catch (NullPointerException e) {
+	        // expected exception was thrown, test passed
+	    }
+	}
+	 
+	    
+	 
+	 
+ 
+ 
+ 
+// intersects test cases
+ 
+ 
+//test to see if intersects returns false if 
+//input lower and upper are outside and greater than range values
+//passes
+ @Test 
+ public void testIntersectsInputLowerAndUpperBoundWhenGreaterThanRangeUpperBound() {
+	 rangeObjectUnderTest = new Range(5, 6);
+  assertFalse( rangeObjectUnderTest.intersects(7,8)); 
+ 
+}
+ 
+ @Test 
+ public void testIntersectsInputValuesAreSubsetOfRange() { 
+	 rangeObjectUnderTest = new Range(6, 12);
+  assertTrue(rangeObjectUnderTest.intersects(10,11)); 
+ 
+}
+ 
+ @Test
+ public void testIntersectsInputsLowerAndUpperBoundWhenLessThanRangeLowerbound() { 
+	 rangeObjectUnderTest = new Range(7,8);
+     assertFalse(rangeObjectUnderTest.intersects(5,6)); 
+ 
+}
+ 
+ @Test
+ public void testIntersectsInputsWhenNegativeAndRangeIsPositive() { 
+	 rangeObjectUnderTest = new Range(1,5);
+     assertFalse(rangeObjectUnderTest.intersects(-5,-1)); 
+ 
+}
+ @Test
+ public void testIntersectsInputIsSubsetOfRangeWhenBothAreNegative() { 
+	 rangeObjectUnderTest = new Range(-10,-5);
+     assertTrue(rangeObjectUnderTest.intersects(-8,-5)); 
+ 
+}
+ 
+ @Test
+ public void testIntersectInputIsSubsetOfRangeWithDecimalPoint() { 
+	 rangeObjectUnderTest = new Range(0,1);
+     assertTrue(rangeObjectUnderTest.intersects(.25,.5)); 
+ 
+}
+ 
+ @Test
+ public void testIntersectInputIsSubsetOfRangeWithNegativeDecimalPoint() { 
+	 rangeObjectUnderTest = new Range(-2.50,-1.5);
+     assertTrue(rangeObjectUnderTest.intersects(-1.75,-0.5)); 
+ 
+}
+  
+ 
+ 
+ //Lower Bound Test cases
+ 
+ 
+ @Test 
+ public void testLowerBoundValueBothPositiveAndEqual() { 
+	 rangeObjectUnderTest = new Range(5, 5);
+  assertEquals("The lower bound value of the range should be 5", 
+     5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ } 
+ 
+ @Test 
+ public void testLowerBoundValueBothPositiveAndNotEqual() { 
+	 rangeObjectUnderTest = new Range(5, 10);
+  assertEquals("The lower bound value of the range should be 5", 
+     5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundValueBothNegativeAndEqual() { 
+	 rangeObjectUnderTest = new Range(-5, -5);
+  assertEquals("The lower bound value of the range should be -5", 
+     -5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundValueBothNegativeAndNotEqual() { 
+	 rangeObjectUnderTest = new Range(-5, -2);
+  assertEquals("The lower bound value of the range should be -5", 
+     -5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundValuesBothZero() { 
+	 rangeObjectUnderTest = new Range(0, 0);
+  assertEquals("The lower bound value of the range should be 0", 
+     0, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundValueZero() { 
+	 rangeObjectUnderTest = new Range(0, 10);
+  assertEquals("The lower bound value of the range should be 0", 
+     0, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundOneNegativeAndOnePositiveValue() { 
+	 rangeObjectUnderTest = new Range(-6, 10);
+  assertEquals("The lower bound value of the range should be -6", 
+     -6, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundBothValuesPositiveDecimalPoint() { 
+	 rangeObjectUnderTest = new Range(2.5, 9.5);
+	 assertEquals("The lower bound value of the range should be 2.5", 
+     2.5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ @Test 
+ public void testLowerBoundBothValuesNegativeDecimalPoint() { 
+	 rangeObjectUnderTest = new Range(-5.5, -2.5);
+	 assertEquals("The lower bound value of the range should be -5.5", 
+     -5.5, rangeObjectUnderTest.getLowerBound(), 0.000000001d); 
+ }
+ 
+ 
+ // ToString Test Cases
+ 
+ 
+ //Test case below fails but should pass 
+ @Test 
+ public void testToStringValuePositiveAndNegative() { 
+	 rangeObjectUnderTest = new Range(-5.0, 5.0);
+     assertEquals("The String representation of this range should be: Range[-5.0,5.0]"
+    		 ,"Range[-5.0,5.0]", rangeObjectUnderTest.toString()); }
+ 
+//Test case below fails but should pass 
+ @Test 
+ public void testToStringValueNegativeAndPositiveDecimalPoint() { 
+	 rangeObjectUnderTest = new Range(-1.5, 1.5);
+     assertEquals("The String representation of this range should be: Range[-1.5,1.5]"
+    		 ,"Range[-1.5,1.5]", rangeObjectUnderTest.toString()); 
+ }
+ 
+//Test case below fails but should pass 
+ @Test 
+ public void testToStringValuesBothPositiveAndNotEqual() { 
+	 rangeObjectUnderTest = new Range(6.0, 10.0);
+     assertEquals("The String representation of this range should be: Range[6.0,10.0]",
+    		 "Range[6.0,10.0]", rangeObjectUnderTest.toString());
+ }
+ 
+
+@Test 
+public void testToStringValuesBothPositiveAndEqual() { 
+	rangeObjectUnderTest = new Range(1.0, 1.0);
+    assertEquals("The String representation of this range should be: Range[1.0,1.0]",
+   		"Range[1.0,1.0]", rangeObjectUnderTest.toString());
+}
+
+@Test 
+public void testToStringValuesBothNegativeAndEqual() { 
+	rangeObjectUnderTest = new Range(-1.0, -1.0);
+    assertEquals("The String representation of this range should be: Range[-1.0,-1.0]",
+   		"Range[-1.0,-1.0]", rangeObjectUnderTest.toString());
+}
+
+//Test case below fails but should pass 
+@Test 
+public void testToStringValuesBothNegativeAndNotEqual() { 
+	rangeObjectUnderTest = new Range(-10.0, -1.0);
+    assertEquals("The String representation of this range should be: Range[-10.0,-1.0]",
+   		"Range[-10.0,-1.0]", rangeObjectUnderTest.toString());
+}
+
+//Test case below fails but should pass 
+@Test 
+public void testToStringValuesBothPositiveDecimalPoints() { 
+	rangeObjectUnderTest = new Range(10.6, 18.3);
+	assertEquals("The String representation of this range should be: Range[10.6,18.3]",
+ 		"Range[10.6,18.3]", rangeObjectUnderTest.toString());
+}
+
+//Test case below fails but should pass 
+@Test 
+public void testToStringValuesBothNegativeDecimalPoints() { 
+	rangeObjectUnderTest = new Range(-3.5, -1.5);
+	assertEquals("The String representation of this range should be: Range[-3.5,-1.5]",
+		"Range[-3.5,-1.5]", rangeObjectUnderTest.toString());
+}
+
+@Test 
+public void testToStringValuesEqualAndZero() { 
+	rangeObjectUnderTest = new Range(0.0, 0.0);
+    assertEquals("The String representation of this range should be: Range[0.0,0.0]",
+   		"Range[0.0,0.0]", rangeObjectUnderTest.toString());
+}
+
+
+// Constrain Test Cases
+
+
+@Test 
+public void testConstrainValueWithinRange() { 
+	rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals("The value returned should be: 0.0",
+   		0.0, rangeObjectUnderTest.constrain(0.0), 0.000000001d);
+}
+
+@Test 
+public void testConstrainValueWithinRangeNegativeValue() { 
+	rangeObjectUnderTest = new Range(-8, -5);
+    assertEquals("The value returned should be: 0.0",
+   		6, rangeObjectUnderTest.constrain(-6), 0.000000001d);
+}
+
+//Test case below fails but should pass 
+@Test 
+public void testConstrainValueBelowRange() { 
+    rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals(-5.0, rangeObjectUnderTest.constrain(-10.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueAboveRange() { 
+    rangeObjectUnderTest = new Range(-2, 5);
+    assertEquals(5.0, rangeObjectUnderTest.constrain(10.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueEqualToLowerBound() { 
+    rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals(-5.0, rangeObjectUnderTest.constrain(-5.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueEqualToUpperBound() { 
+    rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals(5.0, rangeObjectUnderTest.constrain(5.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueWithinRangeCloserToLowerBound() { 
+    rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals(-4.0, rangeObjectUnderTest.constrain(-4.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueWithinRangeCloserToUpperBound() { 
+    rangeObjectUnderTest = new Range(-5, 5);
+    assertEquals(4.0, rangeObjectUnderTest.constrain(4.0), 0.0001);
+}
+
+@Test 
+public void testConstrainValueWithinRangeEquidistantFromBothBounds() { 
+    rangeObjectUnderTest = new Range(0, 5);
+    assertEquals(2.5, rangeObjectUnderTest.constrain(2.5), 0.0001);
+}
+
+ @Test 
+ public void testCentralValueShouldBeZero() { 
+	 rangeObjectUnderTest =new Range(-1,1);
+  assertEquals("The central value of -1 and 1 should be 0", 
+     0, rangeObjectUnderTest.getCentralValue(), 0.000000001d); 
+ } 
+} 
+
